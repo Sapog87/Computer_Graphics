2fd143040ca7e102e6949954abb4fa7c11d9b64b
entcher <49116032+entcher@users.noreply.github.com>
2023-12-12 16:42:45 +0300

    lab 9 floating horizon

---
+++ lab9/floating_horyzon/WinFormsApp2/Algorithm.cs
@@ -0,0 +1,170 @@
+﻿using GraphicsHelper;
+using Point = GraphicsHelper.Point;
+
+namespace AdvancedGraphics
+{
+    public partial class FormPlotting
+    {
+        private const int scaleFactor = 100;
+        private const double threshold = 5;
+        private double yawAngle;
+        private double pitchAngle;
+        private double[] upHorizon;
+        private double[] downHorizon;
+
+        private FloatingPoint GetScaledPoint(Point p)
+        {
+            var res = AffineTransformations.rotate(p, AxisType.Y, yawAngle);
+            res = AffineTransformations.rotate(res, AxisType.X, pitchAngle);
+            var x = Point.worldCenter.X + scaleFactor * res.Xf;
+            if (x < 0 || x >= canvas.Width)
+                return new FloatingPoint(x, scaleFactor * res.Yf, res.Zf * scaleFactor, Visibilty.INVISIBLE);
+            else
+                return new FloatingPoint(x, scaleFactor * res.Yf, res.Zf * scaleFactor, ref upHorizon, ref downHorizon);
+        }
+
+        public void changeViewAngles(double shiftX = 0, double shiftY = 0)
+        {
+            pitchAngle = Math.Clamp(pitchAngle + shiftY, -89.0, 89.0);
+            yawAngle = Math.Clamp(yawAngle + shiftX, -89.0, 89.0);
+        }
+
+        private Color GetColorByVisibility(Visibilty v)
+        {
+            return v switch
+            {
+                Visibilty.VISIBLE_UP => Color.Red,
+                Visibilty.VISIBLE_DOWN => Color.Blue,
+                Visibilty.INVISIBLE => Color.White,
+                _ => Color.White,
+            };
+        }
+
+        void UpdateHorizons(FloatingPoint last, FloatingPoint curr)
+        {
+            if (last.X < 0 || curr.X >= canvas.Width)
+                return;
+
+            if (curr.X - last.X == 0)
+            {
+                upHorizon[curr.X] = Math.Max(upHorizon[curr.X], curr.Yf);
+                downHorizon[curr.X] = Math.Min(downHorizon[curr.X], curr.Yf);
+            }
+            else
+            {
+                var tg = (curr.Yf - last.Yf) / (curr.Xf - last.Xf);
+                for (int x = last.X; x <= curr.X; x++)
+                {
+                    double y = tg * (x - last.Xf) + last.Yf;
+                    upHorizon[x] = Math.Max(upHorizon[x], y);
+                    downHorizon[x] = Math.Min(downHorizon[x], y);
+                }
+            }
+        }
+
+        FloatingPoint FindIntersect(FloatingPoint previous, FloatingPoint curr, bool visible)
+        {
+            double xStep = (curr.Xf - previous.Xf * 1.0) / 20;
+            double yStep = (curr.Yf - previous.Yf * 1.0) / 20;
+            for (int i = 1; i <= 20; i++)
+            {
+                FloatingPoint point = new FloatingPoint(Math.Clamp(previous.Xf + i * xStep, 0, canvas.Width - 1), previous.Yf + i * yStep, curr.Zf,
+                    ref upHorizon, ref downHorizon);
+                if (visible)
+                {
+                    if (point.Visibility == Visibilty.INVISIBLE)
+                        return point;
+                }
+                else
+                {
+                    if (point.Visibility != Visibilty.INVISIBLE)
+                        return point;
+                }
+            }
+
+            return curr;
+        }
+
+
+        void FloatingHorizon(Func<double, double, double> f)
+        {
+            double step = threshold * 2.0 / 75;
+
+            upHorizon = new double[canvas.Width];
+            downHorizon = new double[canvas.Width];
+
+            Array.Fill(upHorizon, double.MinValue);
+            Array.Fill(downHorizon, double.MaxValue);
+
+            var bitmap = new Bitmap(canvas.Width, canvas.Height);
+            var fbitmap = new FastBitmap.FastBitmap(bitmap);
+
+            for (double z = threshold; z >= -threshold; z -= step)
+            {
+                FloatingPoint previous = GetScaledPoint(new Point(-threshold, f(-threshold, z), z));
+                for (double x = -threshold; x <= threshold; x += step)
+                {
+                    FloatingPoint current = GetScaledPoint(new Point(x, f(x, z), z));
+
+                    if (current.Visibility == Visibilty.VISIBLE_UP)
+                    {
+                        if (previous.Visibility != Visibilty.INVISIBLE)
+                        {
+                            Liner.DrawLine(ref fbitmap, previous.toSimple2D(), current.toSimple2D(), GetColorByVisibility(Visibilty.VISIBLE_UP));
+                            UpdateHorizons(previous, current);
+                        }
+                        else
+                        {
+                            var mid = FindIntersect(current, previous, true);
+                            Liner.DrawLine(ref fbitmap, current.toSimple2D(), mid.toSimple2D(), GetColorByVisibility(Visibilty.VISIBLE_UP));
+                            UpdateHorizons(current, mid);
+                        }
+
+                    }
+                    else if (current.Visibility == Visibilty.VISIBLE_DOWN)
+                    {
+                        if (previous.Visibility != Visibilty.INVISIBLE)
+                        {
+                            Liner.DrawLine(ref fbitmap, previous.toSimple2D(), current.toSimple2D(),
+                                GetColorByVisibility(Visibilty.VISIBLE_DOWN));
+                            UpdateHorizons(previous, current);
+                        }
+                        else
+                        {
+                            var mid = FindIntersect(current, previous, true);
+                            Liner.DrawLine(ref fbitmap, current.toSimple2D(), mid.toSimple2D(), GetColorByVisibility(Visibilty.VISIBLE_DOWN));
+                            UpdateHorizons(current, mid);
+                        }
+
+                    }
+                    else
+                    {
+                        if (previous.Visibility == Visibilty.VISIBLE_UP)
+                        {
+                            var mid = FindIntersect(current, previous, false);
+                            Liner.DrawLine(ref fbitmap, previous.toSimple2D(), mid.toSimple2D(), GetColorByVisibility(Visibilty.VISIBLE_UP));
+                            UpdateHorizons(previous, mid);
+                        }
+                        else if (previous.Visibility == Visibilty.VISIBLE_DOWN)
+                        {
+                            var mid = FindIntersect(current, previous, false);
+                            Liner.DrawLine(ref fbitmap, previous.toSimple2D(), mid.toSimple2D(), GetColorByVisibility(Visibilty.VISIBLE_DOWN));
+                            UpdateHorizons(previous, mid);
+                        }
+                    }
+
+                    previous = current;
+                }
+            }
+
+            fbitmap.Dispose();
+            canvas.Image = bitmap;
+        }
+
+
+        void Redraw()
+        {
+            FloatingHorizon(SelectedFunction);
+        }
+    }
+}
---
+++ lab9/floating_horyzon/WinFormsApp2/Plotting.Designer.cs
@@ -0,0 +1,160 @@
+﻿
+namespace AdvancedGraphics
+{
+    partial class FormPlotting
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            canvas = new System.Windows.Forms.PictureBox();
+            groupBox1 = new System.Windows.Forms.GroupBox();
+            radioButton4 = new System.Windows.Forms.RadioButton();
+            radioButton3 = new System.Windows.Forms.RadioButton();
+            radioButton2 = new System.Windows.Forms.RadioButton();
+            radioButton1 = new System.Windows.Forms.RadioButton();
+            radioButton5 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)canvas).BeginInit();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // canvas
+            // 
+            canvas.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            canvas.Dock = System.Windows.Forms.DockStyle.Right;
+            canvas.Location = new System.Drawing.Point(248, 0);
+            canvas.Margin = new System.Windows.Forms.Padding(1);
+            canvas.Name = "canvas";
+            canvas.Size = new System.Drawing.Size(944, 506);
+            canvas.TabIndex = 20;
+            canvas.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(radioButton5);
+            groupBox1.Controls.Add(radioButton4);
+            groupBox1.Controls.Add(radioButton3);
+            groupBox1.Controls.Add(radioButton2);
+            groupBox1.Controls.Add(radioButton1);
+            groupBox1.Location = new System.Drawing.Point(8, 7);
+            groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            groupBox1.Size = new System.Drawing.Size(237, 499);
+            groupBox1.TabIndex = 21;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Функции";
+            // 
+            // radioButton4
+            // 
+            radioButton4.AutoSize = true;
+            radioButton4.Location = new System.Drawing.Point(8, 98);
+            radioButton4.Name = "radioButton4";
+            radioButton4.Size = new System.Drawing.Size(42, 19);
+            radioButton4.TabIndex = 3;
+            radioButton4.TabStop = true;
+            radioButton4.Text = "x*y";
+            radioButton4.UseVisualStyleBackColor = true;
+            radioButton4.CheckedChanged += radioButton4_CheckedChanged;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new System.Drawing.Point(8, 73);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new System.Drawing.Size(91, 19);
+            radioButton3.TabIndex = 2;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "sin(x)*cos(y)";
+            radioButton3.UseVisualStyleBackColor = true;
+            radioButton3.CheckedChanged += radioButton3_CheckedChanged;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new System.Drawing.Point(8, 48);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new System.Drawing.Size(94, 19);
+            radioButton2.TabIndex = 1;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "sin(x)+cos(y)";
+            radioButton2.UseVisualStyleBackColor = true;
+            radioButton2.CheckedChanged += radioButton2_CheckedChanged;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new System.Drawing.Point(8, 23);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new System.Drawing.Size(73, 19);
+            radioButton1.TabIndex = 0;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "x^2+y^2";
+            radioButton1.UseVisualStyleBackColor = true;
+            radioButton1.CheckedChanged += radioButton1_CheckedChanged;
+            // 
+            // radioButton5
+            // 
+            radioButton5.AutoSize = true;
+            radioButton5.Location = new System.Drawing.Point(8, 123);
+            radioButton5.Name = "radioButton5";
+            radioButton5.Size = new System.Drawing.Size(56, 19);
+            radioButton5.TabIndex = 4;
+            radioButton5.TabStop = true;
+            radioButton5.Text = "x*y^2";
+            radioButton5.UseVisualStyleBackColor = true;
+            radioButton5.CheckedChanged += radioButton5_CheckedChanged;
+            // 
+            // FormPlotting
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(1192, 506);
+            Controls.Add(groupBox1);
+            Controls.Add(canvas);
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            KeyPreview = true;
+            Margin = new System.Windows.Forms.Padding(2);
+            MaximizeBox = false;
+            Name = "FormPlotting";
+            StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            Text = "Метод плавающего горизонта";
+            KeyPress += FormPlotting_KeyPress;
+            ((System.ComponentModel.ISupportInitialize)canvas).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox canvas;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+    }
+}
